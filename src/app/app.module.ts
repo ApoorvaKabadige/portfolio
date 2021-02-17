2a353232fe4d235325ce5fd84276ca213fa8b100
vipin <vipin.gopinath@spotcap.com>
2021-02-17 11:49:36 +0530

    css changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './works/works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    AboutComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/works/works.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work',
+  templateUrl: './works.component.html',
+  styleUrls: ['./works.component.scss']
+})
+export class WorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
